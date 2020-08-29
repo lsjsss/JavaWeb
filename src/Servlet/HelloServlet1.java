6c881c0747dbcf83335112aded99cdcbd61d050f
Mr <lsjsss@live.cn>
2020-08-30 01:34:59 +0800

    8.28 class

--- src/Servlet/HelloServlet1.java
+++ src/Servlet/HelloServlet1.java
@@ -1,21 +1,30 @@
 package Servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "HelloServlet1", urlPatterns = {"/HelloServlet1"})
 public class HelloServlet1 extends HttpServlet {
+
+    private static final long SerialVersionUID = 1L;
+
+    public HelloServlet1() {
+        super();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-        System.out.println("welcome");
+//        super.doGet(req, resp);
+//        System.out.println("welcome");
+        resp.getWriter().append("Served at:").append(req.getContextPath());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        super.doGet(req, resp);
     }
 }
